6737ef56a9ad3a8b20f1cd121d8ed874d21e5265
Rouslan Placella <rouslan@placella.com>
2015-12-18 20:32:12 +0000

    Added unit tests

---
+++ test/invalidCron.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+describe('invalidCron', function() {
+  var Cron = require('../src/cron');
+  it('should throw when uninstanciated', function() {
+    var cron = new Cron();
+    assert.throws(function() {
+      cron.toString();
+    }, Error);
+    assert.throws(function() {
+      cron.toArray();
+    }, Error);
+  });
+  it('should throw on invalid cron strings', function() {
+    var invalidCron = [
+      undefined,
+      null,
+      NaN,
+      '',
+      ''
+    ];
+    invalidCron.forEach(function(invalid) {
+      var cron = new Cron();
+      assert.throws(
+          function() {
+            cron.parse(invalid);
+          },
+          Error
+      );
+    });
+  });
+});
