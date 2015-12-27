3da6ff7ff34995073bb9d5fc455fcd69c77966a8
Rouslan Placella <rouslan@placella.com>
2015-12-27 08:49:12 +0000

    Split test

--- test/cron.outputHashes.js
+++ test/cron.outputHashes.js
@@ -4,7 +4,7 @@ var test = require('tape');
 var Cron = require('../src/cron');
 
 test('Should output hashes', function(t) {
-  t.plan(3);
+  t.plan(2);
   var cron = new Cron({
     outputHashes: true
   });
@@ -20,6 +20,10 @@ test('Should output hashes', function(t) {
     'H/5 H(5-20)/5 H(3-9)/3 1-3 1-5',
     'for full intervals and range intervals'
   );
+});
+
+test('Should output hashes for readme', function(t) {
+  t.plan(1);
   var cron = new Cron({
     outputHashes: true,
     outputMonthNames: true,
@@ -28,7 +32,6 @@ test('Should output hashes', function(t) {
   cron.fromString('*/5 9-17/2 * 1-3 1-5');
   t.equal(
     cron.toString(),
-    'H/5 H(10-16)/2 H JAN-MAR MON-FRI',
-    'for readme'
+    'H/5 H(10-16)/2 H JAN-MAR MON-FRI'
   );
 });
